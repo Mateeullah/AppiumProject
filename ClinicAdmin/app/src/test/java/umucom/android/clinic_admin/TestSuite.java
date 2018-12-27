ed151babd11ae047d408b23edd115f6af15ed75a
Mateeullah <bsef15a540@pucit.edu.pk>
2018-12-28 03:39:33 +0500

    Update TestSuite.java

--- ClinicAdmin/app/src/test/java/umucom/android/clinic_admin/TestSuite.java
+++ ClinicAdmin/app/src/test/java/umucom/android/clinic_admin/TestSuite.java
@@ -7,6 +7,7 @@ import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
@@ -49,6 +50,8 @@ public class TestSuite {
         // Set your application's appPackage if you are using any other app.
         capabilities.setCapability("appActivity", "umucom.android.clinic_admin.Activities.AccountActivity");
 
+        capabilities.setCapability("noReset", true);
+
         // Created object of RemoteWebDriver will all set capabilities.
         // Set appium server address and port number in URL string.
         // It will launch calculator app in android device.
@@ -59,16 +62,107 @@ public class TestSuite {
     }
 
     @Test
-    public void loginFirst() {
+    public void loginFailureScenarios() {
+        // Click on DELETE/CLR button to clear result text box before running test.
+        driver.findElements(By.xpath("//android.widget.EditText")).get(0).sendKeys("robot@gmail.com");
+        driver.findElements(By.xpath("//android.widget.EditText")).get(1).sendKeys("robots");
+        driver.findElements(By.xpath("//android.widget.Button")).get(0).click();
+        try {
+            Thread.sleep(1000);
+        }
+        catch (Exception e){}
+        if(driver.findElements(By.xpath("//android.widget.Button")).get(0).getText().equals("MANAGE SLOT")){
+            Assert.assertEquals("Error Message","Login Failure Test Failed");
+        }
+        else{
+            Assert.assertEquals("Login Failure Test Passed","Login Failure Test Passed");
+        }
+
+        driver.findElements(By.xpath("//android.widget.EditText")).get(0).sendKeys("ali@gmail.com");
+        driver.findElements(By.xpath("//android.widget.EditText")).get(1).sendKeys("robot");
+        driver.findElements(By.xpath("//android.widget.Button")).get(0).click();
+        try {
+            Thread.sleep(1000);
+        }
+        catch (Exception e){}
+        if(driver.findElements(By.xpath("//android.widget.Button")).get(0).getText().equals("MANAGE SLOT")){
+            Assert.assertEquals("Error Message","Login Failure Test Failed");
+        }
+
+        else{
+            Assert.assertEquals("Login Failure Test Passed","Login Failure Test Passed");
+        }
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void loginSuccessScenarios() {
         // Click on DELETE/CLR button to clear result text box before running test.
         driver.findElements(By.xpath("//android.widget.EditText")).get(0).sendKeys("robot@gmail.com");
         driver.findElements(By.xpath("//android.widget.EditText")).get(1).sendKeys("robot");
         driver.findElements(By.xpath("//android.widget.Button")).get(0).click();
-        //driver.
-       // driver.findElement(By.id(""+R.id.login_password)).sendKeys("robot");
-        //driver.findElement(By.id(""+R.id.admin_login)).click();
-        // Click on number 2 button.
-        // driver.findElement(By.name("7")).click();
+        try {
+            Thread.sleep(1000);
+        }
+        catch (Exception e){}
+        if(driver.findElements(By.xpath("//android.widget.Button")).get(0).getText().equals("MANAGE SLOT")){
+            Assert.assertEquals("Login Success Test passed","Login Success Test passed");
+        }
+
+        else{
+            Assert.assertEquals("Error Message","Login Success Test Failed");
+        }
+
+
+
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void SignupSuccessScenarios() {
+        // Click on DELETE/CLR button to clear result text box before running test.
+
+        // goto sign up screen
+        driver.findElements(By.xpath("//android.widget.Button")).get(1).click();
+        try {
+            Thread.sleep(500);
+        }
+        catch (Exception e){}
+
+        //fill form
+        driver.findElements(By.xpath("//android.widget.EditText")).get(3).sendKeys("ajmal123");
+        driver.findElements(By.xpath("//android.widget.EditText")).get(0).sendKeys("ajmal");
+        driver.findElements(By.xpath("//android.widget.EditText")).get(1).sendKeys("hotguy37@gmail.com");
+        driver.findElements(By.xpath("//android.widget.EditText")).get(2).sendKeys("ajmal123");
+
+        driver.navigate().back();
+        //press sign up
+        driver.findElements(By.xpath("//android.widget.Button")).get(0).click();
+
+        try {
+            Thread.sleep(1000);
+        }
+        catch (Exception e){}
+
+        //go back
+
+        driver.navigate().back();
+        //fill same login info
+        driver.findElements(By.xpath("//android.widget.EditText")).get(0).sendKeys("hotguy37@gmail.com");
+        driver.findElements(By.xpath("//android.widget.EditText")).get(1).sendKeys("ajmal123");
+        driver.findElements(By.xpath("//android.widget.Button")).get(0).click();
+        try {
+            Thread.sleep(1000);
+        }
+        catch (Exception e){}
+        if(driver.findElements(By.xpath("//android.widget.Button")).get(0).getText().equals("MANAGE SLOT")){
+            Assert.assertEquals("Sign up Success Test passed","Sign up Success Test passed");
+        }
+
+        else{
+            Assert.assertEquals("Error Message","Sign up Success Test Failed");
+        }
+
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     }
 
@@ -79,4 +173,6 @@ public class TestSuite {
 
 
 }
+
+
 
